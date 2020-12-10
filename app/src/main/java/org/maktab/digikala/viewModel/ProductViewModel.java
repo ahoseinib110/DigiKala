90bcc588a5205d7e08881f9941320a6fdf48be21
ahoseinib110 <ahoseinib110@gmail.com>
2020-12-10 15:53:48 +0330

    .

---
+++ app/src/main/java/org/maktab/digikala/viewModel/ProductViewModel.java
@@ -0,0 +1,12 @@
+package org.maktab.digikala.viewModel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class ProductViewModel extends AndroidViewModel {
+    public ProductViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
